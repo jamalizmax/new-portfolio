d071e91e10e6a5a3543227c827aece0920f45dde
jamalizmax corporate <128382385+jamalizmax@users.noreply.github.com>
2023-03-31 12:34:56 +0700

    menambhkan form contct

--- js/script.js
+++ js/script.js
@@ -21,6 +21,16 @@ $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
 
   $(".jumbotron img").css({
-    transform: "translate(0px, " + wScroll / 4 + "%)",
+    transform: "translate (0px, " + wScroll / 4 + "%)",
   });
+});
+
+$("#myButton").on("click", function () {
+  var button = $(this);
+  button.html('<i class="fa fa-spinner fa-spin"></i> Loading...');
+  setTimeout(function () {
+    button.html("kirim");
+    alert("Pesan Anda sudah terkirim!");
+    $("form")[0].reset();
+  }, 3000);
 });
